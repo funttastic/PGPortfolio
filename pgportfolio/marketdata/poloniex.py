c59186e79fb7880f8cad837015216404c8a8e4c5
Danilo Araújo Silva <danilo.silva@funttastic.com>
2023-11-13 18:28:34 +0100

    Updating poliniex class.

--- pgportfolio/marketdata/poloniex.py
+++ pgportfolio/marketdata/poloniex.py
@@ -11,46 +11,68 @@ else:
     from urllib import urlencode
 
 minute = 60
-hour = minute*60
-day = hour*24
-week = day*7
-month = day*30
-year = day*365
+hour = minute * 60
+day = hour * 24
+week = day * 7
+month = day * 30
+year = day * 365
 
-# Possible Commands
-PUBLIC_COMMANDS = ['returnTicker', 'return24hVolume', 'returnOrderBook', 'returnTradeHistory', 'returnChartData', 'returnCurrencies', 'returnLoanOrders']
+# Novos Endpoints da API
+PUBLIC_COMMANDS = {
+    'returnTicker': 'markets/ticker24h',
+    'return24hVolume': 'markets/ticker24h',
+    'returnOrderBook': 'markets/{symbol}/orderBook',
+    'returnTradeHistory': 'markets/{symbol}/trades',
+    'returnChartData': 'markets/{symbol}/candles',
+    'returnCurrencies': 'currencies',
+    'returnLoanOrders': 'loanOrders'
+}
 
 class Poloniex:
     def __init__(self, APIKey='', Secret=''):
         self.APIKey = APIKey.encode()
         self.Secret = Secret.encode()
-        # Conversions
         self.timestamp_str = lambda timestamp=time.time(), format="%Y-%m-%d %H:%M:%S": datetime.fromtimestamp(timestamp).strftime(format)
         self.str_timestamp = lambda datestr=self.timestamp_str(), format="%Y-%m-%d %H:%M:%S": int(time.mktime(time.strptime(datestr, format)))
-        self.float_roundPercent = lambda floatN, decimalP=2: str(round(float(floatN) * 100, decimalP))+"%"
+        self.float_roundPercent = lambda floatN, decimalP=2: str(round(float(floatN) * 100, decimalP)) + "%"
 
-        # PUBLIC COMMANDS
-        self.marketTicker = lambda x=0: self.api('returnTicker')
-        self.marketVolume = lambda x=0: self.api('return24hVolume')
-        self.marketStatus = lambda x=0: self.api('returnCurrencies')
-        self.marketLoans = lambda coin: self.api('returnLoanOrders',{'currency':coin})
-        self.marketOrders = lambda pair='all', depth=10:\
-            self.api('returnOrderBook', {'currencyPair':pair, 'depth':depth})
-        self.marketChart = lambda pair, period=day, start=time.time()-(week*1), end=time.time(): self.api('returnChartData', {'currencyPair':pair, 'period':period, 'start':start, 'end':end})
-        self.marketTradeHist = lambda pair: self.api('returnTradeHistory',{'currencyPair':pair}) # NEEDS TO BE FIXED ON Poloniex
-
-    #####################
-    # Main Api Function #
-    #####################
     def api(self, command, args={}):
-        """
-        returns 'False' if invalid command or if no APIKey or Secret is specified (if command is "private")
-        returns {"error":"<error message>"} if API error
-        """
-        if command in PUBLIC_COMMANDS:
-            url = 'https://poloniex.com/public?'
-            args['command'] = command
-            ret = urlopen(Request(url + urlencode(args)))
-            return json.loads(ret.read().decode(encoding='UTF-8'))
-        else:
-            return False
+      base_url = 'https://api.poloniex.com/'
+      if command in PUBLIC_COMMANDS:
+          url = base_url + PUBLIC_COMMANDS[command]
+          if '{symbol}' in url:
+              url = url.format(symbol=args.get('currencyPair'))
+
+          headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+          print(url + '?' + urlencode(args))
+          req = Request(url + '?' + urlencode(args), headers=headers)
+
+          ret = urlopen(req)
+          return json.loads(ret.read().decode(encoding='UTF-8'))
+      else:
+          return False
+
+    def marketTicker(self, x=0):
+        return self.api('returnTicker')
+
+    def marketVolume(self, x=0):
+        return self.api('return24hVolume')
+
+    def marketStatus(self, x=0):
+        return self.api('returnCurrencies')
+
+    def marketLoans(self, coin):
+        return self.api('returnLoanOrders',{'currency':coin})
+
+    def marketOrders(self, pair='all', depth=10):
+        return self.api('returnOrderBook', {'currencyPair':pair, 'depth':depth})
+
+    def marketChart(self, pair, period=day, start=time.time()-(week*1), end=time.time()):
+        return self.api('returnChartData', {'currencyPair':pair, 'period':period, 'start':start, 'end':end})
+
+    def marketTradeHist(self, pair):
+        return self.api('returnTradeHistory',{'currencyPair':pair})
+
+# Exemplo de uso
+# poloniex = Poloniex()
+# print(poloniex.marketTicker())
